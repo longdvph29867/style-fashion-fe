09cbc527a9bf7ef86db69999245d03f8fde6778d
longdv ph29867 <longdvph29867@gmail.com>
2024-06-02 23:50:17 +0700

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,7 @@ function App() {
           <Route path="products/add" element={<AddProduct />} />
           <Route path="products/update/:id" element={<UpdateProduct />} />
         </Route>
+
         {/* Các route khác nếu có */}
       </Routes>
     </>
